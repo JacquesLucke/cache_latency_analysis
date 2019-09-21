271fe7ce91f67d484fb8e32e8832506877872a02
Jacques Lucke <mail@jlucke.com>
2019-09-21 11:22:26 +0200

    link list latency

---
+++ link_list_latency.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <chrono>
+#include <sstream>
+#include <fstream>
+
+using uint = unsigned int;
+
+struct ListItem {
+    ListItem *next = nullptr;
+    char pad[64 - sizeof(ListItem *)];
+};
+
+using Clock = std::chrono::high_resolution_clock;
+using TimePoint = Clock::time_point;
+using Nanoseconds = std::chrono::nanoseconds;
+
+#pragma optimize("", off)
+
+uint do_not_optimize_value = 0;
+
+template<class T> void do_not_optimize_away(T &&datum)
+{
+    do_not_optimize_value += (int)&datum;
+}
+
+#pragma optimize("", on)
+
+__declspec(noinline) double measure_time_per_load(ListItem *begin)
+{
+    static volatile ListItem *s_final;
+
+    uint loop_iterations = 1'000'000;
+    uint total_loads = loop_iterations * 10;
+
+    ListItem *current = begin;
+
+    TimePoint start = Clock::now();
+
+    for (uint i = 0; i < loop_iterations; i++) {
+        current = current->next;
+        current = current->next;
+        current = current->next;
+        current = current->next;
+        current = current->next;
+
+        current = current->next;
+        current = current->next;
+        current = current->next;
+        current = current->next;
+        current = current->next;
+    }
+
+    TimePoint end = Clock::now();
+
+    do_not_optimize_away(current);
+
+    Nanoseconds duration = end - start;
+    double ns_per_load = (double)duration.count() / (double)total_loads;
+    return ns_per_load;
+}
+
+double measure_time(std::vector<ListItem *> &randomized_items, uint amount)
+{
+    for (uint i = 0; i < amount; i++) {
+        randomized_items[i]->next = randomized_items[(i + 1) % amount];
+    }
+
+    double duration_sum = 0;
+    uint iterations = 20;
+    for (uint i = 0; i < iterations; i++) {
+        duration_sum += measure_time_per_load(randomized_items[0]);
+    }
+    double average_duration = duration_sum / (double)iterations;
+    return average_duration;
+}
+
+int main(int argc, char const *argv[])
+{
+    std::cout << sizeof(ListItem) << '\n';
+    uint allocation_amount = 10'000'000;
+
+    std::vector<ListItem *> all_list_items;
+    all_list_items.reserve(allocation_amount);
+
+    for (uint i = 0; i < allocation_amount; i++) {
+        all_list_items.push_back(new ListItem());
+    }
+
+    std::shuffle(all_list_items.begin(),
+                 all_list_items.end(),
+                 std::default_random_engine());
+
+    std::stringstream output_stream;
+    for (uint i = 10; i < 1'000'000; i *= 1.2) {
+        double time = measure_time(all_list_items, i);
+        output_stream << "Measurement"
+                      << ";" << i << ";" << time << '\n';
+        std::cout << i << ": \t" << time << " ns\n";
+    }
+
+    std::ofstream file(
+        "C:\\Users\\jacques\\Documents\\performance_tests\\benchmark_result."
+        "csv");
+    file << output_stream.str();
+    file.close();
+
+    std::cout << do_not_optimize_value << "\n";
+
+    return 0;
+}
--- plotting.py
+++ plotting.py
@@ -1,40 +1,37 @@
-# import plotly.graph_objects as go
+import plotly.graph_objects as go
 import plotly.express as px
 from collections import defaultdict
 
-with open("/home/jacques/Documents/performance_tests/benchmark_results.csv") as f:
+with open("C:\\Users\\jacques\\Documents\\performance_tests\\benchmark_result_save.csv") as f:
     loaded_results = f.read()
 
 data = defaultdict(list)
 
 fig = go.Figure()
 
-data_by_function = defaultdict(list)
+data_by_name = defaultdict(lambda: ([], []))
 
 for line in loaded_results.splitlines():
-    parts = line.split(";")
-    data_by_function[parts[1]].append(line)
+    name, x_value, y_value = line.split(";")
+    data_by_name[name][0].append(int(x_value) * 64)
+    data_by_name[name][1].append(float(y_value))
 
-for function, lines in data_by_function.items():
-    x_values = []
-    y_values = []
-    for line in lines:
-        parts = line.split(";")
-        x_values.append(f"{int(parts[0]):,}")
-        y_values.append(parts[-1])
-    fig.add_trace(go.Scatter(x=x_values, y=y_values, name=function))
+for name, (x_values, y_values) in data_by_name.items():
+    fig.add_trace(go.Scatter(name=name, x=x_values, y=y_values))
 
 fig.update_layout(
     barmode="group",
-    xaxis_title="Number of Non-Zeros (of 10,000,000)",
-    yaxis_title="Time in Milliseconds",
+    xaxis_title="Total Bytes",
+    yaxis_title="Time in Nanoseconds per Load",
     #xaxis_type="category",
     xaxis_tickformat=",.",
     legend_orientation="h",
     legend_x=0,
     legend_y=1.15,
     margin_t=0,
-    showlegend=True)
-fig.update_yaxes(range=[0, 7], dtick=1)
-fig.write_image("/home/jacques/Documents/performance_tests/bit_iteration.png", width=800, height=350)
+    showlegend=False,
+    xaxis_type="log",
+    xaxis_nticks=5)
+#fig.update_yaxes(range=[0, 7], dtick=1)
+#fig.write_image("/home/jacques/Documents/performance_tests/bit_iteration.png", width=800, height=350)
 fig.show()
